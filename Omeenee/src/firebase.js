533fca29c8343e1f6b52f824db39567a8b18c319
Arthur Chima <keengarthurchima@gmail.com>
2023-11-19 18:14:54 +0100

    updated

--- Omeenee/src/firebase.js
+++ Omeenee/src/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDM_86dSMuQeLfsPCP5DGi4L8UDx8oD5Eo",
   authDomain: "omeenee-59739.firebaseapp.com",
